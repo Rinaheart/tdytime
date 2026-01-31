bdbf3b7b5b3fca39683247a7b97d7b8f68922c8f
RinaSQ <rinaheart@gmail.com>
2026-01-31 11:15:46 +0700

    1115

--- i18n.ts
+++ i18n.ts
@@ -3,6 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import viTranslation from './locales/vi.json';
 import enTranslation from './locales/en.json';
 
+import { APP_VERSION } from './constants';
+
 const resources = {
     vi: { translation: viTranslation },
     en: { translation: enTranslation }
@@ -15,7 +17,10 @@ i18n
         lng: localStorage.getItem('language') || 'vi', // Default to Vietnamese
         fallbackLng: 'vi',
         interpolation: {
-            escapeValue: false // React already escapes
+            escapeValue: false, // React already escapes
+            defaultVariables: {
+                version: APP_VERSION
+            }
         }
     });
 
